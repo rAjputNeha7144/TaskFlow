644d6cadf81560f3ce231db038538e45b0ba1da7
Neha Rajput <inrajput0987@gmail.com>
2026-06-05 22:35:43 +0530

    renamed html file

---
+++ TaskFlow.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+  let button = document.getElementById("getStartedBtn");
+
+  button.addEventListener("click", function () {
+    alert("Welcome to TaskFlow!");
+  });
+});
